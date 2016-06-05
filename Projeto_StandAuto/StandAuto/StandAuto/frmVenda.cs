e8098a3ae84159c2425fad663f2ba2d39e9aa054
roudry2 <roudry2@hotmail.com>
2016-06-06 00:53:32 +0100

    okok

--- Projeto_StandAuto/StandAuto/StandAuto/frmVenda.cs
+++ Projeto_StandAuto/StandAuto/StandAuto/frmVenda.cs
@@ -17,6 +17,33 @@ namespace StandAuto
             InitializeComponent();
         }
 
+        private void vendaSetBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.vendaSetBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.standAutoDataSet);
 
+        }
+
+        private void frmVenda_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'standAutoDataSet.VendaSet' table. You can move, or remove it, as needed.
+            this.vendaSetTableAdapter.Fill(this.standAutoDataSet.VendaSet);
+            // TODO: This line of code loads data into the 'standAutoDataSet.ClienteSet' table. You can move, or remove it, as needed.
+            this.clienteSetTableAdapter1.Fill(this.standAutoDataSet.ClienteSet);
+
+
+        }
+
+        private void nomeLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnNovoCli_Click(object sender, EventArgs e)
+        {
+            frmCliente cli = new frmCliente();
+            cli.ShowDialog();
+        }
     }
 }
